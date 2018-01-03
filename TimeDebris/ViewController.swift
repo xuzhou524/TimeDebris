2a3d7a32c903ab4f4b6ba97d8492cfb5ef08e27f
xuzhou524 <xuuohz@163.com>
2018-01-03 17:57:04 +0800

    添加部分按钮icon

--- TimeDebris/ViewController.swift
+++ TimeDebris/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let imageView = UIImageView.init(frame: CGRect(x:0, y:0, width:UIScreen.main.bounds.size.width, height:320))
+        imageView.image = UIImage.init(named: "image3")
+        self.view.addSubview(imageView)
+        
+        let addImageView = UIImageView.init(frame: CGRect(x:UIScreen.main.bounds.size.width/2.0 - 30, y:UIScreen.main.bounds.size.height - 60 - 60, width:60, height:60))
+        addImageView.image = UIImage.init(named: "tianjia")
+        self.view.addSubview(addImageView)
+        
+        let liebiaoImageView = UIImageView.init(frame: CGRect(x:30, y:UIScreen.main.bounds.size.height - 25 - 30, width:25, height:25))
+        liebiaoImageView.image = UIImage.init(named: "liebiao")
+        self.view.addSubview(liebiaoImageView)
+        
+        let userImageView = UIImageView.init(frame: CGRect(x:UIScreen.main.bounds.size.width - 25 - 30, y:UIScreen.main.bounds.size.height - 25 - 30, width:25, height:25))
+        userImageView.image = UIImage.init(named: "user")
+        self.view.addSubview(userImageView)
         
     }
 
