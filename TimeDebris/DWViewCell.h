c8f1ea3918c784e2573db7f704839ce93cd1d820
xuzhou524 <xuuohz@163.com>
2018-01-03 16:39:34 +0800

    bak

---
+++ TimeDebris/DWViewCell.h
@@ -0,0 +1,16 @@
+//
+//  DWViewCell.h
+//  CardSlide
+//
+//  Created by DavidWang on 15/11/25.
+//  Copyright © 2015年 DavidWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DWViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *showImg;
+@end
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
