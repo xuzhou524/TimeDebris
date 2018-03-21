57b434cc9da8d2b1c1fe0163ae99512758f7ecf0
xuzhou524 <xuuohz@163.com>
2018-03-21 17:11:29 +0800

    编辑

--- TimeDebris/Controllers/EditorViewController.swift
+++ TimeDebris/Controllers/EditorViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class EditorViewController: UIViewController {
+    
+    let headTapView : UIView = {
+        let headTapView = UIView()
+        headTapView.backgroundColor = XZSwiftColor.white
+        return headTapView
+    }()
+    
+    let titleTextField : UITextField = {
+        let titleTextField = UITextField()
+        titleTextField.placeholder = "今天做了什么事"
+        titleTextField.font = XZClient.XZFont2(size: 16)
+        return titleTextField
+    }()
+    
+    let describeTextView : UITextField = {
+        let describeTextView = UITextField()
+        describeTextView.placeholder = "写下你的描述"
+        describeTextView.font = XZClient.XZFont2(size: 16)
+        return describeTextView
+    }()
+    
+    let iconImageView : UIImageView = {
+        let iconImageView = UIImageView()
+        iconImageView.backgroundColor = XZSwiftColor.orange
+        return iconImageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,35 +42,99 @@ class EditorViewController: UIViewController {
         
         self.navigationController?.navigationBar.isHidden = true
         
-        let tableView = UITableView()
-        tableView.backgroundColor = XZSwiftColor.xzGlay230
-        tableView.separatorStyle = .none
-        self.view.addSubview(tableView)
-        tableView.snp.makeConstraints({ (make) -> Void in
-            make.left.right.bottom.top.equalTo(self.view)
-        });
+        self.setUpHeadView()
+        self.setUpEditorView()
+        self.setUpToobarView()
+    
+    }
+    
+    func setUpHeadView() {
         
-        let headTapView = UIView()
-        headTapView.backgroundColor = XZSwiftColor.clear
-        self.view.addSubview(headTapView)
-        headTapView.snp.makeConstraints({ (make) -> Void in
+        self.view.addSubview(self.headTapView)
+        self.headTapView.snp.makeConstraints({ (make) -> Void in
             make.left.right.top.equalTo(self.view)
             make.height.equalTo(64)
+        });
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "留下微印"
+        titleLabel.textColor = XZSwiftColor.xzGlay50
+        titleLabel.font = XZClient.XZFont2(size: 18)
+        self.headTapView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints({ (make) -> Void in
+            make.centerX.equalTo(self.headTapView)
+            make.centerY.equalTo(self.headTapView).offset(10)
+        });
+    }
+    
+    func setUpToobarView() {
+        let tapView = UIView()
+        tapView.backgroundColor = XZSwiftColor.clear
+        self.view.addSubview(tapView)
+        tapView.snp.makeConstraints({ (make) -> Void in
+            make.left.right.bottom.equalTo(self.view)
+            make.height.equalTo(100)
         });
         
         let backImageView = UIImageView()
         backImageView.image = UIImage.init(named: "guanbi")
         backImageView.isUserInteractionEnabled = true
-        headTapView.addSubview(backImageView)
+        tapView.addSubview(backImageView)
         backImageView.snp.makeConstraints({ (make) -> Void in
-            make.left.equalTo(headTapView).offset(20)
-            make.centerY.equalTo(headTapView).offset(12)
-            make.width.height.equalTo(21)
+            make.center.equalTo(tapView)
+            make.width.height.equalTo(25)
         });
         let tapAction = UITapGestureRecognizer.init(target: self, action: #selector(UserViewController.backActionClick))
         backImageView.addGestureRecognizer(tapAction)
+    }
+    
+    func setUpEditorView() {
+        self.view.addSubview(self.titleTextField)
+        self.titleTextField.snp.makeConstraints({ (make) -> Void in
+            make.left.equalTo(self.view).offset(20)
+            make.right.equalTo(self.view).offset(-20)
+            make.top.equalTo(self.headTapView.snp.bottom).offset(10)
+            make.height.equalTo(50)
+        });
+        
+        let calendarImageView = UIImageView()
+        calendarImageView.image = UIImage.init(named: "Calendar_21")?.withRenderingMode(.alwaysTemplate)
+        calendarImageView.tintColor = XZSwiftColor.xzGlay129
+        calendarImageView.isUserInteractionEnabled = true
+        self.view.addSubview(calendarImageView)
+        calendarImageView.snp.makeConstraints({ (make) -> Void in
+            make.left.equalTo(self.titleTextField)
+            make.top.equalTo(self.titleTextField.snp.bottom).offset(10)
+            make.width.height.equalTo(24)
+        });
+        
+        self.view.addSubview(self.describeTextView)
+        self.describeTextView.snp.makeConstraints({ (make) -> Void in
+            make.left.right.equalTo(self.titleTextField)
+            make.top.equalTo(calendarImageView.snp.bottom).offset(10)
+            make.height.equalTo(50)
+        });
+        
+        self.view.addSubview(self.iconImageView)
+        self.iconImageView.snp.makeConstraints({ (make) -> Void in
+            make.left.right.equalTo(self.titleTextField)
+            make.top.equalTo(self.describeTextView.snp.bottom).offset(10)
+            make.height.equalTo(120)
+        });
+        
+        let saveButton = UIButton()
+        //calendarImageView.image = UIImage.init(named: "guanbi")
+        saveButton.backgroundColor = XZSwiftColor.xzGlay129
+        self.view.addSubview(saveButton)
+        saveButton.snp.makeConstraints({ (make) -> Void in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(self.iconImageView.snp.bottom).offset(25)
+            make.width.equalTo(200)
+            make.height.equalTo(40)
+        });
         
     }
+    
     @objc func backActionClick() {
         let transition = CATransition()
         transition.duration = 0.4
