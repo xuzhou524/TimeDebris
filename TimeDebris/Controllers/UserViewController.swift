78a1f54591d7c152ee9d853a4747e85030ef7115
xuzhou524 <xuuohz@163.com>
2018-11-19 09:49:02 +0800

    bak

--- TimeDebris/Controllers/UserViewController.swift
+++ TimeDebris/Controllers/UserViewController.swift
@@ -104,6 +104,8 @@ class UserViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
             return 100
         }else if (indexPath as NSIndexPath).row == 1 {
             return 15
+        }else if (indexPath as NSIndexPath).row == 3 {
+            return 0
         }
         return 55
     }
